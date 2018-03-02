fdc45400f5ae91abc15510d2a2558e8bf49c971b
brycegaming <36393016+brycegaming@users.noreply.github.com>
2018-03-02 11:36:00 -0700

    Updated With Dylan's Edits

---
+++ Assets/scripts/Bullet.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    [SerializeField] GameObject explosion;
+
+    public float bulletSpeed = 40;
+    GameObject bow;
+    
+    Rigidbody2D rb2d;
+    GunUpdate bowScript;
+
+    float maxTime;
+    float time;
+
+
+    void Start()
+    {
+        maxTime = 5.0f;
+        time = 0;
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+
+        if (time > maxTime)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+        if (true)
+        {
+
+        }
+
+        if (rb2d.velocity.y >= 0f)
+        {
+            float angleToBe = Vector2.Angle(Vector2.right, rb2d.velocity);
+            transform.rotation = Quaternion.Euler(0, 0, angleToBe);
+        }
+        else
+        {
+            float angleToBe = -1f * Vector2.Angle(Vector2.right, rb2d.velocity);
+            transform.rotation = Quaternion.Euler(0, 0, angleToBe);
+        }
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //create an explosion object and destroy this object
+
+        GameObject newExplosion = Instantiate(explosion);
+        newExplosion.transform.position = transform.position;
+
+        Destroy(gameObject);
+    }
+    
+}
---
+++ Assets/scripts/PlayerDeath.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour {
+    private Vector3 spawnPoint;
+    private Rigidbody2D body;
+
+    void Awake()
+    {
+        spawnPoint = transform.position;
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Respawn")
+        {
+            transform.position = spawnPoint;
+            body.velocity = new Vector3(0, 0, 0);
+        }
+    }
+}
